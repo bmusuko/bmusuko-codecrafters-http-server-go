84b38f6e980c86ed2b199a60bcf10a6d86a2337d
Bram Musuko Panjaitan <bram.musuko@bytedance.com>
2024-08-03 22:37:21 +0700

    feat: reply curl
    Change-Id: I00676f5c13147713527c304491dd48d9cf68e510

--- app/server.go
+++ app/server.go
@@ -18,9 +18,19 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		// Block until we receive an incoming connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection
+		go handleClient(conn)
 	}
+}
+
+func handleClient(conn net.Conn) {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
